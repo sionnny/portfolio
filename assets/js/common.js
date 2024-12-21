641272e78841b93350d41d4575571655154057a7
sionnny <karilion1009@gmail.com>
2024-12-21 20:52:53 +0900

    Add files via upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,14 +3,47 @@ $(document).ready(function () {
 });
 
 //페이지하단 스크롤탑 버튼
-$(window).scroll(function(){
-	if ($(this).scrollTop() > 300){
-		$('.btn_gotop').show();
-	} else{
-		$('.btn_gotop').hide();
-	}
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+    $(".btn_gotop").show();
+  } else {
+    $(".btn_gotop").hide();
+  }
 });
-$('.btn_gotop').click(function(){
-	$('html, body').animate({scrollTop:0},400);
-	return false;
+$(".btn_gotop").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    400
+  );
+  return false;
 });
+
+// // home 으로 바로 이동
+// $(document).ready(function () {
+//   $(".menu_home").click(function () {
+//     var offset = $("html, body").offset();
+//     $("html").animate(
+//       {
+//         scrollTop: scrollTop-59,
+//       },
+//       300
+//     );
+//   });
+// });
+
+//about으로 바로 이동
+// $(document).ready(function () {
+//   $(".menu_about").click(function () {
+//     var offset = $(".section02").offset();
+//     $("html").animate(
+//       {
+//         scrollTop: offset.top,
+//       },
+//       300
+//     );
+//   });
+// });
+
+
